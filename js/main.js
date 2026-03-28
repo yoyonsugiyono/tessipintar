6f6c3929baf82f07fb26fe0eda8d47cb791e053b
yoyonsugiyono <102265433+yoyonsugiyono@users.noreply.github.com>
2026-03-28 23:19:51 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@
 
 import { auth, db } from './config/firebase.js';
 import { setupAuth, initLoginForm, handleLogout, activeTahun, activeSemester } from './services/auth.js';
+import { loadMasterData } from './services/db-master.js';
+import { setupNavigation, buildSidebarNav, switchMenu } from './ui/navigation.js';
 
 function init() {
     console.log("[DEBUG] Aplikasi Si PINTAR mulai diinisialisasi...");
     
-    // Setel tanggal di pojok kanan atas
     const currentDateEl = document.getElementById('current-date');
     if (currentDateEl) {
         currentDateEl.textContent = new Date().toLocaleDateString('id-ID', { 
@@ -14,13 +15,13 @@ function init() {
         });
     }
 
-    // 1. Siapkan Form Login
+    // 1. Inisialisasi Auth & Sidebar Event
+    setupNavigation();
     initLoginForm((user) => {
-        // Jika login sukses, jalankan ini
         showApp(user);
     });
 
-    // 2. Siapkan Tombol Logout
+    // 2. Tombol Logout
     const btnLogout = document.getElementById('btn-logout');
     if (btnLogout) {
         btnLogout.onclick = () => {
@@ -31,25 +32,30 @@ function init() {
         }
     }
 
-    // 3. Mulai koneksi Firebase Auth
-    setupAuth((firebaseUser) => {
-        console.log("[DEBUG] Data master & pengguna siap digunakan!");
-        // (Nanti fungsi load master data dan load nilai kita panggil di sini)
+    // 3. Tarik Master Data saat terhubung
+    setupAuth(async (firebaseUser) => {
+        await loadMasterData();
+        // Nanti kita tambahkan "loadDataNilai()" di sini
     });
+
+    // 4. Jadikan fungsi switchMenu global agar bisa dipanggil dari atribut 'onclick' di HTML
+    window.switchMenu = switchMenu;
 }
 
-// Fungsi untuk berpindah dari layar Login ke Layar Utama
 function showApp(user) {
     document.getElementById('login-view').classList.add('hidden');
     document.getElementById('main-view').classList.remove('hidden');
     
-    // Set identitas di sidebar & header
     document.getElementById('auth-user-name').textContent = user.username.split(',')[0];
     document.getElementById('auth-user-role').textContent = user.role;
     document.getElementById('page-subtitle-name').textContent = user.username.split(',')[0];
     document.getElementById('display-periode').innerHTML = `<i class="ph ph-calendar-check"></i> TA. ${activeTahun} - Semester ${activeSemester}`;
     
-    console.log(`[DEBUG] Berhasil masuk sebagai ${user.role}`);
+    // Bangun menu sidebar sesuai role (Admin/Guru/Wakasek)
+    buildSidebarNav();
+    
+    // Secara otomatis arahkan ke dashboard
+    switchMenu('dashboard');
 }
 
 window.onload = init;
